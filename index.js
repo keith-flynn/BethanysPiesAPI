57fdee84d20a377cb214cb4f337f447aefe258b4
Keith Flynn <flynn.keith@gmail.com>
2023-09-30 19:13:21 -0400

    Add all from lesson through GET
    Everything included for an express server using node.js to run a REST
    API. Base for starting a new REST API.

---
+++ index.js
@@ -0,0 +1,30 @@
+// Bring in the express server and create application
+let express = require('express');
+let app = express();
+let pieRepo = require('./repos/pieRepo');
+
+// Use the express Router object
+let router = express.Router();
+
+//Create GET to return a list of all pies
+router.get('/', function (req, res, next) {
+  pieRepo.get(function (data) {
+    res.status(200).json({
+      "status": 200,
+      "statusText": "OK",
+      "message": "All pies are retrieved.",
+      "isKeithCool": true,
+      "data": data
+    });
+  }, function(err) {
+    next(err);
+  });
+});
+
+// Configure router so all routes are prefixed with /api/v1
+app.use('/api/', router);
+
+//Create server to listen on port 5000
+var server = app.listen(5000, function () {
+  console.log('Node server is running on http://localhost:5000..');
+});
