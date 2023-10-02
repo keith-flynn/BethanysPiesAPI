0c65ac612a10126519c624c6383fc54d56527776
Keith Flynn <flynn.keith@gmail.com>
2023-10-01 20:16:21 -0400

    Add delete function
    Method to delete entry by id.

--- index.js
+++ index.js
@@ -112,6 +112,35 @@ router.put('/:id', function (req, res, next) {
   });
 });
 
+router.delete('/:id', function (req, res, next) {
+  pieRepo.getById(req.params.id, function (data) {
+    if (data) {
+      // Attempt to delete data
+      pieRepo.delete(req.params.id, function (data) {
+        res.status(200).json({
+          "status": 200,
+          "statusText": "OK",
+          "message": "Pie '" + req.params.id + "' is deleted.",
+          "data": data
+        })
+      })
+    }
+    else {
+      res.status(404).json({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The Pie '" + req.params.id + "' could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The Pie '" + req.params.id + "' could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  })
+})
+
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 
